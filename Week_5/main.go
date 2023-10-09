37392312b82f4e453da9c3b596310f24e5d92299
huniiverse <huniivers@gmail.com>
2023-10-09 20:04:42 +0900

    v1 test

---
+++ Week_5/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix()) // get the current data and time as an integer
+	answer := rand.Intn(100) + 1 // random integer number (1 ~ 100)
+	fmt.Println("Guess Number Game~")
+	fmt.Println(answer)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("You have", 10-i, "chances~")
+		fmt.Print("Input guess number : ")
+		inputNumberString, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		inputNumberString = strings.TrimSpace(inputNumberString)
+		inputNumber, err := strconv.Atoi(inputNumberString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if inputNumber == answer {
+			fmt.Println("Great! U got number. congratulations~") // Answer is higher~
+			break
+		} else if inputNumber < answer {
+			fmt.Println("Guess number is lower then answer") // Answer is higher~
+		} else if inputNumber > answer {
+			fmt.Println("Guess number is higher then answer") // Answer is lower
+		}
+	}
+}
