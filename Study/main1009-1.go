2c11a88422f3924c8fe0c1b566ddfd53f008fb76
huniiverse <huniivers@gmail.com>
2023-10-09 20:58:05 +0900

    v2 study

--- Study/main1009-1.go
+++ Study/main1009-1.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+// func main() {
+// 	fmt.Println("안녕하세요")
+// 	var number int
+// 	number = 4
+// 	fmt.Println(number)
+// }
+
 func main() {
 	fmt.Println("안녕하세요")
+	number := 4
+	fmt.Println(number)
 }
+
+// 변수 선언하기
+// var quantity int -> var [변수명] [변숫값의 타입]
+// 변수 선언 -> 변수에 값 할당 -> 변수 사용하기
+// 변수에 들어갈 값을 알고 있으면 동시에 할당 가능 -> var number int = 4
+// 값을 할당하지 않고 선언한 변수는 타입에 대한 제로값으로 초기화
+
+// 변수 단축 선언 :=
+// number := 4
+// 단축 변수 선언을 사용하면 변수의 타입은 할당한 값의 타입으로 자동 지정됨
